49a37943c2190cc3d9c1e9f961b9806591e12a5f
Sam Fletcher <samfletcher@ou.edu>
2018-10-10 14:13:26 -0500

    Updated calculator java doc

---
+++ src/me/samfletcher/Tests/AllTests.java
@@ -0,0 +1,12 @@
+package me.samfletcher.Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CalculatorTest.class, GUITest.class })
+public class AllTests
+{
+
+}
